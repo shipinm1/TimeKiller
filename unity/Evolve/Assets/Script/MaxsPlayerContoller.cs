addf2cb139b9a3e7152199dc25bd806cc1964aee
shipinm1 <shipin1@ualberta.ca>
2018-08-01 04:43:48 -0600

    asd

---
+++ unity/Evolve/Assets/Script/MaxsPlayerContoller.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class maxsPlayerController : MonoBehaviour {
+
+    public int hitpoint;
+
+    public void Hp()
+    {
+        hitpoint = 100;
+    }
+
+}
